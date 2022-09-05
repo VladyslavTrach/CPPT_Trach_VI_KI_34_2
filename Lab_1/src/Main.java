72fe07d891c3f1c1d595d590fe836d13dbdc5a30
vladyslav_t28 <vladyslav.trach.ki.2020@lpnu.ua>
2022-09-05 20:15:27 +0300

    Lab_1

---
+++ Lab_1/src/Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String message = new String();
+        message = "Trach Vladyslav KI-34";
+
+        System.out.println(message);
+    }
+}
